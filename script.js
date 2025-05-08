eb2d80be6eb7853b6df8f9874e9c08ce321cceff
Bharath S J <166963664+Bharath-S-J@users.noreply.github.com>
2025-05-08 16:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener('click', async ()=>{
+	let data =document.getElementById("text").value
+	let delay=document.getElementById("delay").value
+	let fetcheddata=await resolvepromise(data,delay)
+	document.getElementById("output").innerText=fetcheddata
+})
+
+function resolvepromise(data,delay) {
+	return new Promise((resolve)=>{
+		setTimeout(()=>resolve( data),delay)
+	});
+	
+}
+
+
+
+
+
+
+
+
